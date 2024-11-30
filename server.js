f7e88088d149d659febe7ed91587dcf309a285bb
VIKAS YADAV <34761088+7054company@users.noreply.github.com>
2024-11-30 20:40:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const __dirname = dirname(__filename);
 dotenv.config();
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -40,3 +40,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
